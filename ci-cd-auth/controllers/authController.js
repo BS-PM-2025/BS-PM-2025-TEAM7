68db3f9f3ba085a0ca19a8f5b1f234dda75614fc
mosabkh12 <159187725+mosabkh12@users.noreply.github.com>
2025-05-21 16:46:32 +0300

    BSPM25T7-45: Upload video - Lecturer

--- ci-cd-auth/controllers/authController.js
+++ ci-cd-auth/controllers/authController.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const JWT_SECRET = process.env.JWT_SECRET || "default_secret";
+// ✅ SIGNUP (Hashing will be done in model's pre-save)
 
 // ✅ SIGNUP (Hashing will be done in model's pre-save)
 exports.signup = async (req, res) => {
